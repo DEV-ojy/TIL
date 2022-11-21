aae65ef0a4fa5d2d772c7f7a7cd1034becd83adb
오주연 <80239748+DEV-ojy@users.noreply.github.com>
2022-11-21 11:32:57 +0900

    Create 과일장수.py

---
+++ coding_test/programmers/1level/과일장수/과일장수.py
@@ -0,0 +1,10 @@
+def solution(k, m, score):
+    answer = 0 
+    score.sort(reverse=True)
+    for i in range(0,len(score),m):
+        tmp = score[i:i+m]
+        if len(tmp)==m:
+            answer += min(tmp) * m
+
+
+    return answer
